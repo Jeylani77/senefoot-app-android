2e3d676041535735bf466964b798ce2804729db5
jey <khadrenjaay@gmail.com>
2020-07-21 13:55:08 +0000

    Create Fragment for Equipe

--- app/src/main/java/com/example/senefoot/MainActivity.java
+++ app/src/main/java/com/example/senefoot/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.senefoot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.senefoot.adapter.EquipeAdapter;
+import com.example.senefoot.fragment.EquipeFragment;
+import com.example.senefoot.fragment.RecontreFragment;
 import com.example.senefoot.model.Equipe;
 import com.example.senefoot.rest.ApiClient;
 import com.example.senefoot.rest.ApiInterface;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,69 +36,42 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_matches:
+                    showFragment(new RecontreFragment());
+
+                    return true;
+                case R.id.nav_teams:
+                    showFragment(new EquipeFragment());
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.rv_team);
 
-        mRecyclerView.setHasFixedSize(true);
-        //mLayoutManager = new GridLayoutManager(MetsActivity.this,2,GridLayoutManager.VERTICAL,false);
-        mLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL,false);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bnv_main);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        showFragment(new RecontreFragment());
 
-        //it get the title of menu initialized before in MenuActivity
-        met=getIntent().getStringExtra("MET");
-
-        //Integrate Retrofit
-        apiService = ApiClient.getClient().create(ApiInterface.class);
-        Call<List<Equipe>> call= apiService.listeEquipes("senior");
-        /*Log the URL called*/
-        Log.d("URL Called", call.request().url() + "");
-        call.enqueue(new Callback<List<Equipe>>() {
-            @Override
-            public void onResponse(Call<List<Equipe>> call, Response<List<Equipe>> response) {
-                Log.d("Tous les équipes", response.body()+"");
-                Toast.makeText(MainActivity.this, response.body()+"", Toast.LENGTH_SHORT).show();
-                equipeList = response.body();
-                showEquipe();
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Equipe>> call, Throwable t) {
-                Log.e("Error",t.getMessage());
-
-            }
-        });
-/*
-        call.enqueue(new Callback<List<E>>() {
-            @Override
-            public void onResponse(Call<List<Met>> call, Response<List<Met>> response) {
-                metsRetrofit= response.body();
-
-                Log.d("response cmd", response.body()+"");
-                swipeRefresh_projet_list.setRefreshing(false);
-
-
-                showMet();
-            }
-
-            @Override
-            public void onFailure(Call<List<Met>> call, Throwable t) {
-                Log.e("Error",t.getMessage());
-            }
-        });*/
     }
 
-    // this method manage the view of Equipes
-    public void showEquipe() {
-        equipeAdapter =new EquipeAdapter(equipeList);
-        mRecyclerView.setAdapter(equipeAdapter);
-
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fl_container_main, fragment)
+                .commit();
     }
 }
